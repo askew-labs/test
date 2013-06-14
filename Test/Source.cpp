f8eebaf6a216e466c2d895ea49be5580dd07d2d2
tim a askew <askew_design@hotmail.com>
2013-06-13 22:26:37 -0500

    Send again

---
+++ Test/Source.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main()
+{
+	string mouse = "mouse";
+	string cat = "cat";
+	string ball = "string";
+
+	cout<<mouse<<" chases "<<cat;
+
+	cout<<ball<<endl;
+
+	cout<<ball<<endl;
+
+
+
+
+
+
+
+
+	return 0;
+}
